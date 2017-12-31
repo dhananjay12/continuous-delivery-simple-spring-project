ab39e99dc150902da80b9023069e975b96ea6c32
Dhananjay Singh <dhananjay1204@gmail.com>
2017-12-31 17:19:49 +0530

    simple spring boot project

---
+++ src/main/java/com/example/continuousdeliverysimplespringproject/ContinuousDeliverySimpleSpringProjectApplication.java
@@ -0,0 +1,12 @@
+package com.example.continuousdeliverysimplespringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContinuousDeliverySimpleSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContinuousDeliverySimpleSpringProjectApplication.class, args);
+	}
+}
